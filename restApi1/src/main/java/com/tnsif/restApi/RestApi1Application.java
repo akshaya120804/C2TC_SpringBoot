4078e97acaffae6c77590d96820400c2ab358cb7
AKSHAYA <AKSHAYA@LAPTOP-FOEJL238>
2025-09-08 20:06:56 +0530

    Commit

---
+++ restApi1/src/main/java/com/tnsif/restApi/RestApi1Application.java
@@ -0,0 +1,13 @@
+package com.tnsif.restApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApi1Application.class, args);
+	}
+
+}
