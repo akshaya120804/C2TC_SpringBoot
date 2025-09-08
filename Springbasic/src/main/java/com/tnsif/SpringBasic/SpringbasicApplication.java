15753e194870938b7db561b44e601f92ee47ebe1
AKSHAYA <AKSHAYA@LAPTOP-FOEJL238>
2025-09-08 20:09:39 +0530

    Commit

---
+++ Springbasic/src/main/java/com/tnsif/SpringBasic/SpringbasicApplication.java
@@ -0,0 +1,14 @@
+package com.tnsif.SpringBasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbasicApplication.class, args);
+		System.out.println("Hello world !");
+	}
+
+}
