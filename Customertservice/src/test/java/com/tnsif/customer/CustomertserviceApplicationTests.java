6cf7379a274126e74e4906c818634c8d30918dd8
AKSHAYA <AKSHAYA@LAPTOP-FOEJL238>
2025-09-17 18:24:16 +0530

    Commit

---
+++ Customertservice/src/test/java/com/tnsif/customer/CustomertserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnsif.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomertserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
